e97df76dca3fa769f8c5fee6a9ed1ed03b428d01
Angel Gonzalez <angel.gonzalez.2007@gmail.com>
2020-11-22 04:20:27 -0600

    Issue #169 | added win/lose screen * Remove unused combat win screen * Added combat animation for satelite * Added sound for win and lose screen

---
+++ win/Win.gd
@@ -0,0 +1,15 @@
+extends ColorRect
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if !event.is_pressed():
+		return
+	elif event.is_action("ui_accept"):
+		_on_Restart_pressed()
+
+func _on_Restart_pressed():
+	Global.goto_scene(Global.Scene.GROUND_CONTROL)
+	self.queue_free()
