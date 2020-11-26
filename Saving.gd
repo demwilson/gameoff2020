70277744f91986f200206462e71f0a7beebdc456
Alexander Wilson <demwilson@gmail.com>
2020-11-26 09:41:10 -0600

    #213 | Save Time * Changed saving window time from 1 to 0.5 seconds. * Updated the text in a few places.

--- Saving.gd
+++ Saving.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 onready var info = $CanvasLayer/ColorRect/Label
-const SAVING_WAIT_TIME = 1
+const SAVING_WAIT_TIME = 0.5
 const TIME_TO_READ = 0.5
 
 # Called when the node enters the scene tree for the first time.
